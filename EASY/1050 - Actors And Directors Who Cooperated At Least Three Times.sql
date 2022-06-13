9fd5d30d4f822f81721c750976459e6d99c8fb38
Freeva Racheal <38135441+freevs@users.noreply.github.com>
2022-06-13 09:23:23 +0530

    Add files via upload

---
+++ EASY/1050 - Actors And Directors Who Cooperated At Least Three Times.sql
@@ -0,0 +1,18 @@
+create table ActorDirector(actor_id int, director_id int, timestamp int);
+insert into ActorDirector(actor_id, director_id, timestamp ) values(1, 1, 0), (1, 1, 1), (1, 1, 2), (1, 2, 3), (1, 2, 4), (2, 1, 5), (2, 1, 6);
+
+select actor_id, director_id
+from ActorDirector
+group by actor_id, director_id
+having count(*)>=3;
+
+																	
+                                                                    
+                                                                  
+                                                                    
+                                                                   
+                                                                    
+
+
+
+ 
