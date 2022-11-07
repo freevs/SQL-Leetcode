f6662e0fed5f73a68aede0ec13a02009dfbbb1be
Freeva Racheal <38135441+freevs@users.noreply.github.com>
2022-11-07 14:47:02 +0530

    Update 176 - Second Highest Salary.sql

--- EASY/176 - Second Highest Salary.sql
+++ EASY/176 - Second Highest Salary.sql
@@ -4,9 +4,7 @@ insert into employees_176(id, salary)values(1, 100), (2, 200), (3, 300);
 Select max(salary) from employees_176 where salary !=
 (select max(salary) from employees_176);
 
-select
-    (select distinct Salary
-        from employees_176
-        order by salary desc
-        limit 1 offset 1) 
-as SecondHighestSalary;
+select distinct Salary
+from employees_176
+order by salary desc
+limit 1 offset 1;
