a6196fe291cf1c85651a4806b2fd20226c4d46ed
Freeva Racheal <38135441+freevs@users.noreply.github.com>
2022-11-09 16:43:23 +0530

    Update 1303 - Find The Team Size.sql

--- EASY/1303 - Find The Team Size.sql
+++ EASY/1303 - Find The Team Size.sql
@@ -6,3 +6,7 @@ SELECT employee_id, COUNT(employee_id) OVER (PARTITION BY team_id) AS team_size
 FROM Employee_1303
 order by employee_id;
 
+select e1.employee_id, count(e2.employee_id) as team_size
+from Employee_1303 e1
+inner join Employee e2 on e1.team_id = e2.team_id
+group by e1.employee_id, e2.team_id
