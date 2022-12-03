fd5294ef9ae049f210ad5c1b4627c868c1bd130a
Freeva Racheal <38135441+freevs@users.noreply.github.com>
2022-12-03 13:51:05 +0530

    Add files via upload

---
+++ MEDIUM/1699 - Number Of Calls Between Two Persons.sql
@@ -0,0 +1,24 @@
+CREATE TABLE Calls_1699 (
+  from_id INT,
+  to_id INT,
+  duration INT
+);
+INSERT INTO Calls_1699
+  (from_id,to_id,duration)
+VALUES
+  (1,2,59),
+  (2,1,11),
+  (1,3,20),
+  (3,4,100),
+  (3,4,200),
+  (3,4,200),
+  (4,3,499);
+ 
+with cte as
+(select case when from_id < to_id then from_id else to_id end as person1,
+case when from_id <to_id then to_id else from_id end as person2, duration
+from Calls_1699)
+
+select person1, person2, count(*) as call_count, sum(duration)
+from cte
+group by person1, person2
