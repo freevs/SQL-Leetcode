02ae60065105198e6990b2a5fd83d8c907da1235
Freeva Racheal <38135441+freevs@users.noreply.github.com>
2023-04-24 19:35:49 +0530

    Delete 1158 - Market Analysis I.sql

--- MEDIUM/1158 - Market Analysis I.sql
+++
@@ -1,9 +0,0 @@
-select * from items_1158;
-select * from orders_1158;
-select * from users_1158;
-
-select u.user_id as buyer_id, u.join_date , sum(case when order_id is not null then 1 else 0 end) as orders_in_2019
-from users_1158 u left join orders_1158 o
-on u.user_id=o.buyer_id and year(order_date)=2019
-group by user_id
-
