be423ed27ecab501739bb1388524cdc4db193a0f
Freeva Racheal <38135441+freevs@users.noreply.github.com>
2023-04-24 19:42:50 +0530

    Update 1158 - Market Analysis I.sql

--- MEDIUM/1158 - Market Analysis I.sql
+++ MEDIUM/1158 - Market Analysis I.sql
@@ -2,7 +2,7 @@ select * from items_1158;
 select * from orders_1158;
 select * from users_1158;
 
-select u.user_id as buyer_id, u.join_date, count(order_id) 
+select u.user_id as buyer_id, u.join_date, count(order_id) as orders_in_2019
 from users_1158 u left join orders_1158 o
 on u.user_id=o.buyer_id and year(order_date) = 2019
 group by user_id;
