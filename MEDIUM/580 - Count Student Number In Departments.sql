8dece18c7d017b8e0b580fc3d2c2ca562f574acc
Freeva Racheal <38135441+freevs@users.noreply.github.com>
2024-05-13 14:34:00 +0530

    Update 580 - Count Student Number In Departments.sql

--- MEDIUM/580 - Count Student Number In Departments.sql
+++ MEDIUM/580 - Count Student Number In Departments.sql
@@ -1,17 +1,17 @@
-CREATE TABLE student_580
+CREATE TABLE students_580
    (student_id int, student_name varchar(25),  gender char, dept_id int
 );
-INSERT INTO student_580 
+INSERT INTO students_580 
   (student_id, student_name, gender, dept_id) 
 VALUES
   (1, 'Jack', 'M', 1),
   (2, 'Jane', 'F', 1),
   (3, 'Mark', 'M', 2);
   
-CREATE TABLE dep_548 (
+CREATE TABLE deps_548 (
   dept_id int, dept_name VARCHAR(255)
 );
-INSERT INTO dep_548 
+INSERT INTO deps_548 
   (dept_id ,dept_name) 
 VALUES 
   (1, 'Engineering'),
@@ -19,7 +19,8 @@ VALUES
   (3, 'Law');
   
 select dept_name, count(student_id) as student_number
-from student_580 e right join dep_548 d
+from students_580 e right join deps_548 d
 on e.dept_id = d.dept_id
-group by d.dept_id;
+group by d.dept_id
+order by student_number desc, d.dept_name;
   
