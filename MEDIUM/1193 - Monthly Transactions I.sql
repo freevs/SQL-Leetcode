dc4427b56fc3f833fb5b67cd7acfb41196ceeb21
Freeva Dsouza <38135441+freevs@users.noreply.github.com>
2024-10-15 18:23:25 +0530

    Update 1193 - Monthly Transactions I.sql

--- MEDIUM/1193 - Monthly Transactions I.sql
+++ MEDIUM/1193 - Monthly Transactions I.sql
@@ -19,7 +19,7 @@ Select date_format(trans_date,"%Y-%m") as month, country,
 count(id) as trans_count,
 sum(case when state='approved' then 1 else 0 end) as approved_count,
 sum(amount) as trans_total_amount, 
-sum(case when state='approved' then amount else 0 end) as approved_count
+sum(case when state='approved' then amount else 0 end) as approved_total_amount
 from transactions_1193
 group by month, country
   
